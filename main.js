8c74c655fa8a4db027fbf40768b1dd108240c2ca
Gabby Suazo <gabby.suazo@gmail.com>
2020-07-20 18:48:40 -0400

    Add javascript function to make the box animate around page

---
+++ main.js
@@ -0,0 +1,6 @@
+$('#box').click(function() {
+    $('#box').animate({left: "50%"}, 2000);
+    $('#box').animate({top: "50%"}, 2000);
+    $('#box').animate({left: "0"}, 2000);
+    $('#box').animate({top: "0"}, 2000);
+});
